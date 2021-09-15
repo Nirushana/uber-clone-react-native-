737a9bc3466e42c8008b1cffd0cab0fb8f890f51
Nirushana <kavinda.n.t@gmail.com>
2021-09-15 19:09:56 +0530

    Maps

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -2,12 +2,12 @@ import React from "react";
 import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
 import tw from "tailwind-react-native-classnames";
 import NavOptions from "../components/NavOptions";
-import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
-import { GOOGLE_MAPS_APIKEY } from '@env';
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { GOOGLE_MAPS_APIKEY } from "@env";
 import { useDispatch } from "react-redux";
+import { setDestination, setOrigin } from "../slices/navSlices";
 
 const HomeScreen = () => {
-
   const dispatch = useDispatch();
 
   return (
@@ -24,26 +24,33 @@ const HomeScreen = () => {
           }}
         />
         <GooglePlacesAutocomplete
-        placeholder="Where From" 
-        styles={{
-          container: {
-            flex: 0,
-          },
-          textInput: {
-            fontSize: 18,
-          }
-        }}
-        onPress={(data, details)=> {
+          placeholder="Where From"
+          styles={{
+            container: {
+              flex: 0,
+            },
+            textInput: {
+              fontSize: 18,
+            },
+          }}
+          onPress={(data, details) => {
+            dispatch(
+              setOrigin({
+                location: details.geometry.location,
+                description: data.description,
+              })
+            );
 
-        }}
-        fetchDetails={true}
-        returnKeyType={"search"}
-        enablePoweredByContainer={false}
-        minLength={2}
-        query={{
-          key: GOOGLE_MAPS_APIKEY,
-          language: "en",
-        }}
+            dispatch(setDestination(null));
+          }}
+          fetchDetails={true}
+          returnKeyType={"search"}
+          enablePoweredByContainer={false}
+          minLength={2}
+          query={{
+            key: GOOGLE_MAPS_APIKEY,
+            language: "en",
+          }}
           nearbyPlacesAPI="GooglePlacesSearch"
           debounce={400}
         />

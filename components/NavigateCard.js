ee1441602c502ad8cd8e03a63e004492de4d41ca
Nirushana <kavinda.n.t@gmail.com>
2021-09-17 20:25:14 +0530

    MapView

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -1,14 +1,42 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import tw from "tailwind-react-native-classnames";
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
+import { GOOGLE_MAPS_APIKEY } from "@env";
 
 const NavigateCard = () => {
-    return (
+  return (
+    <SafeAreaView style={tw`bg-white flex-1`}>
+      <Text style={tw`text-center py-5 text-xl`}>Good Day, Kavinda</Text>
+      <View style={tw`border-t border-gray-200 flex-shrink`}>
         <View>
-            <Text>I am the Stack</Text>
+            <GooglePlacesAutocomplete 
+                placeholder="Where to"
+                nearbyPlacesAPI="GooglePlaceSearch"
+                debounce={400}
+            />
         </View>
-    )
-}
+      </View>
+    </SafeAreaView>
+  );
+};
 
-export default NavigateCard
+export default NavigateCard;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "white",
+        paddingTop: 20,
+        flex: 0,
+    },
+    textInput:{
+        backgroundColor: "#DDDDDF",
+        borderRadius: 0,
+        fontSize: 18,
+    },
+    textInputContainer: {
+        paddingHorizontal: 20,
+        paddingBottom: 0,
+    }
+});

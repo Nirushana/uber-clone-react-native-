cd8a7cca12bf52c6ffbd73aa8ddba0f62d7ad888
Nirushana <kavinda.n.t@gmail.com>
2021-09-17 20:36:47 +0530

    Destination

--- components/NavigateCard.js
+++ components/NavigateCard.js
@@ -4,18 +4,43 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import tw from "tailwind-react-native-classnames";
 import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 import { GOOGLE_MAPS_APIKEY } from "@env";
+import { useDispatch } from "react-redux";
+import { setDestination } from "../slices/navSlices";
+import { useNavigation } from "@react-navigation/native";
 
 const NavigateCard = () => {
+  const dispatch = useDispatch();
+  const navigation = useNavigation();
   return (
     <SafeAreaView style={tw`bg-white flex-1`}>
-      <Text style={tw`text-center py-5 text-xl`}>Good Day, Kavinda</Text>
+      <Text style={tw`text-center py-5 text-xl`}>
+        Good Day, Kavinda Nirushana
+      </Text>
       <View style={tw`border-t border-gray-200 flex-shrink`}>
         <View>
-            <GooglePlacesAutocomplete 
-                placeholder="Where to"
-                nearbyPlacesAPI="GooglePlaceSearch"
-                debounce={400}
-            />
+          <GooglePlacesAutocomplete
+            styles={toInputBoxStyles}
+            placeholder="Where to"
+            nearbyPlacesAPI="GooglePlaceSearch"
+            debounce={400}
+            fetchDetails={true}
+            returnKeyType={"search"}
+            minLength={2}
+            onPress={(data, details = null) => {
+              dispatch(
+                setDestination({
+                  location: details.geometry.location,
+                  description: data.description,
+                })
+              );
+              navigation.navigate("RideOptionsCard");
+            }}
+            enablePoweredByContainer={false}
+            query={{
+              key: GOOGLE_MAPS_APIKEY,
+              language: "en",
+            }}
+          />
         </View>
       </View>
     </SafeAreaView>
@@ -24,19 +49,19 @@ const NavigateCard = () => {
 
 export default NavigateCard;
 
-const styles = StyleSheet.create({
-    container: {
-        backgroundColor: "white",
-        paddingTop: 20,
-        flex: 0,
-    },
-    textInput:{
-        backgroundColor: "#DDDDDF",
-        borderRadius: 0,
-        fontSize: 18,
-    },
-    textInputContainer: {
-        paddingHorizontal: 20,
-        paddingBottom: 0,
-    }
+const toInputBoxStyles = StyleSheet.create({
+  container: {
+    backgroundColor: "white",
+    paddingTop: 20,
+    flex: 0,
+  },
+  textInput: {
+    backgroundColor: "#DDDDDF",
+    borderRadius: 0,
+    fontSize: 18,
+  },
+  textInputContainer: {
+    paddingHorizontal: 20,
+    paddingBottom: 0,
+  },
 });

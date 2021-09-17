553c8d174edbda31639d6a1429ecfa2ddc5b8e30
Nirushana <kavinda.n.t@gmail.com>
2021-09-17 19:46:55 +0530

    MapView

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,34 +1,42 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 import tw from "tailwind-react-native-classnames";
-import Map from '../components/Map';
+import Map from "../components/Map";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import NavigateCard from '../components/NavigateCard';
+import NavigateCard from "../components/NavigateCard";
+import RideOptionsCard from "../components/RideOptionsCard";
 
 const MapScreen = () => {
   const Stack = createNativeStackNavigator();
 
+  return (
+    <View>
+      <View style={tw`h-1/2`}>
+        <Map />
+      </View>
+      <View style={tw`h-1/2`}>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="NavigateCard"
+            component={NavigateCard}
+            options={{
+              headerShown: false,
+            }}
+          />
 
-    return (
-        <View>
-            <View style={tw`h-1/2`}>
-            <Map />
-            </View>
-            <View style={tw`h-1/2`}>
-                <Stack.Navigator>
-                  <Stack.Screen 
-                    name="NavigateCard"
-                    component={NavigateCard}
-                    options={{
-                        headerShown:false
-                    }}
-                  />  
-                </Stack.Navigator>
-            </View>
-        </View>
-    )
-}
+          <Stack.Screen
+            name="RideOptionsCard"
+            component={RideOptionsCard}
+            options={{
+              headerShown: false,
+            }}
+          />
+        </Stack.Navigator>
+      </View>
+    </View>
+  );
+};
 
-export default MapScreen
+export default MapScreen;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});

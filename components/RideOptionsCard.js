f42e4c023852246da89c8e3a7bedb5a11ebfbd72
Nirushana <kavinda.n.t@gmail.com>
2021-10-05 02:25:20 +0530

    RideOptions

--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -1,33 +1,41 @@
-import React from "react";
-import { StyleSheet, Text, View, TouchableOpacity, FlatList, Image } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  FlatList,
+  Image,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import tw from "tailwind-react-native-classnames";
 import { Icon } from "react-native-elements";
 import { useNavigation } from "@react-navigation/native";
 
-const data =[
-    {
-        id: "Uber-X-123",
-        title: "UberX",
-        multiplier: 1,
-        image: "https://links.papareact.com/3pn",
-    },
-    {
-        id: "Uber-XL-456",
-        title: "UberXL",
-        multiplier: 1.2,
-        image: "https://links.papareact.com/5w8",
-    },
-    {
-        id: "Uber-LUX-789",
-        title: "UberLUX",
-        multiplier: 1.75,
-        image: "https://links.papareact.com/7pf",
-    }
-]
+const data = [
+  {
+    id: "Uber-X-123",
+    title: "UberX",
+    multiplier: 1,
+    image: "https://links.papareact.com/3pn",
+  },
+  {
+    id: "Uber-XL-456",
+    title: "UberXL",
+    multiplier: 1.2,
+    image: "https://links.papareact.com/5w8",
+  },
+  {
+    id: "Uber-LUX-789",
+    title: "UberLUX",
+    multiplier: 1.75,
+    image: "https://links.papareact.com/7pf",
+  },
+];
 
 const RideOptionsCard = () => {
   const navigation = useNavigation();
+  const [selected, setSelected] = useState(null);
   return (
     <SafeAreaView style={tw`bg-white flex-grow`}>
       <View>
@@ -42,23 +50,26 @@ const RideOptionsCard = () => {
 
       <FlatList
         data={data}
-        keyExtractor={item => item.id}
-        renderItem={({item: { id, image, title, multiplier, }}) => (
-            <TouchableOpacity style={tw`flex-row justify-between items-center px-10`}>
-              <Image
-                style={{
-                  width: 100,
-                  height: 100,
-                  resizeMode: "contain",
-                }}
-                source={{ uri: image }}
-              />
-              <View style={tw`-ml-6`}>
-                <Text style={tw`text-xl font-semibold`}>{title}</Text>
-                <Text>Travel time...</Text>
-              </View>
-              <Text style={tw`text-xl`}>LKR 120</Text>
-            </TouchableOpacity>
+        keyExtractor={(item) => item.id}
+        renderItem={({ item: { id, image, title, multiplier }, item }) => (
+          <TouchableOpacity
+            onPress={() => setSelected(item)}
+            style={tw`flex-row justify-between items-center px-10 ${id === selected?.id && 'bg-gray-200'}`}
+          >
+            <Image
+              style={{
+                width: 100,
+                height: 100,
+                resizeMode: "contain",
+              }}
+              source={{ uri: image }}
+            />
+            <View style={tw`-ml-6`}>
+              <Text style={tw`text-xl font-semibold`}>{title}</Text>
+              <Text>Travel time...</Text>
+            </View>
+            <Text style={tw`text-xl`}>LKR 120</Text>
+          </TouchableOpacity>
         )}
       />
     </SafeAreaView>
